3f86e8c94bf337ae81af0413bbeae76dc82b76f1
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-06-15 22:44:05 -0700

    Update irgen.js

--- src/compiler/irgen.js
+++ src/compiler/irgen.js
@@ -518,6 +518,10 @@ class ScriptTreeGenerator {
                 kind: 'op.atan',
                 value
             };
+            case 'atan2': return {
+                kind: 'op.atan2',
+                value
+            };
             case 'ln': return {
                 kind: 'op.ln',
                 value
