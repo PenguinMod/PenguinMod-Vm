88508f4bb27812a29c6bb7a9d7ccfc1b055a260a
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-07-18 21:22:39 -0700

    revert  block-shape.js

--- src/extension-support/block-shape.js
+++ src/extension-support/block-shape.js
@@ -27,11 +27,6 @@ const BlockShape = {
      * Output shape: plus (custom).
      */
     PLUS: 5,
-
-    /**
-     * Output shape: custom (user-defined in Blockly.BlockSvg).
-     */
-    CUSTOM: -1,
 };
 
 module.exports = BlockShape;
