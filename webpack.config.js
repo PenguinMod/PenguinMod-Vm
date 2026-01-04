4f3892b098d3f85ae0b5b55c022d51e6ed2228b4
RedMan13 <godslayerakp@gmail.com>
2026-01-03 20:25:11 -0800

    dont copy these, they dont exist

--- webpack.config.js
+++ webpack.config.js
@@ -138,12 +138,6 @@ module.exports = [
                 from: 'node_modules/scratch-blocks/media',
                 to: 'media'
             }, {
-                from: 'node_modules/scratch-storage/dist/web'
-            }, {
-                from: 'node_modules/scratch-render/dist/web'
-            }, {
-                from: 'node_modules/scratch-svg-renderer/dist/web'
-            }, {
                 from: 'src/playground'
             }])
         ])
