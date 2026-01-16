c7a61a995044bf40e933a8d3a4d5cec957a0dfb0
jwklong <72522395+jwklong@users.noreply.github.com>
2026-01-16 16:02:44 +0000

    jwScope has

--- src/extensions/jwScope/index.js
+++ src/extensions/jwScope/index.js
@@ -47,6 +47,15 @@ const jwScope = {
         return null
     },
 
+    has(array, name) {
+        for (let i = array.length-1; i >= 0; i--) {
+            if (name in array[i]) {
+                return true
+            }
+        }
+        return false
+    }
+
     reset(array) {
         for (let i = array.length-1; i >= 0; i--) {
             array[i] = {}
@@ -130,11 +139,23 @@ class Extension {
                         }
                     },
                 },
+                "---",
                 {
                     opcode: "get",
                     blockType: BlockType.REPORTER,
                     text: "get [NAME]",
                     allowDropAnywhere: true,
+                    arguments: {
+                        NAME: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "var"
+                        }
+                    },
+                },
+                {
+                    opcode: "has",
+                    blockType: BlockType.BOOLEAN,
+                    text: "is [NAME] defined?",
                     arguments: {
                         NAME: {
                             type: ArgumentType.STRING,
@@ -217,6 +238,10 @@ class Extension {
                     kind: 'input',
                     name: generator.descendInputOfBlock(block, 'NAME')
                 }),
+                has: (generator, block) => ({
+                    kind: 'input',
+                    name: generator.descendInputOfBlock(block, 'NAME')
+                }),
                 reset: (generator, block) => ({
                     kind: 'stack'
                 }),
@@ -242,6 +267,9 @@ class Extension {
                 },
                 get: (node, compiler, imports) => {
                     return new imports.TypedInput(`vm.jwScope.get(jwScope, ${compiler.descendInput(node.name).asString()})`, imports.TYPE_UNKNOWN)
+                },
+                has: (node, compiler, imports) => {
+                    return new imports.TypedInput(`vm.jwScope.has(jwScope, ${compiler.descendInput(node.name).asString()})`, imports.TYPE_BOOLEAN)
                 },
                 reset: (node, compiler, imports) => {
                     compiler.source += `vm.jwScope.reset(jwScope);\n`
