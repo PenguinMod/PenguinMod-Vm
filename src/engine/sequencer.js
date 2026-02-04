fed3caf2a2d8fddd698a568d61a6fefe135bebde
the-can-of-soup <91906096+the-can-of-soup@users.noreply.github.com>
2026-02-04 13:23:02 -0500

    Add before and after step events (2/2)

--- src/engine/sequencer.js
+++ src/engine/sequencer.js
@@ -103,9 +103,11 @@ class Sequencer {
             // Attempt to run each thread one time.
             const threads = this.runtime.threads;
             this.runtime.emit('BEFORE_TICK', threads);
-            for (this.activeThreadIndex = 0; this.activeThreadIndex < threads.length; this.activeThreadIndex++) {
+            for (this.activeThreadIndex = 0; this.activeThreadIndex < threads.length; this.activeThreadIndex++,
+                 this.runtime.emit('AFTER_STEP', this.activeThread)) {
                 const i = this.activeThreadIndex;
                 const activeThread = this.activeThread = threads[i];
+                this.runtime.emit('BEFORE_STEP', activeThread);
                 // Check if the thread is done so it is not executed.
                 if (activeThread.stack.length === 0 ||
                     activeThread.status === Thread.STATUS_DONE) {
