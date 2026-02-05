6e4bb738eb27fa0932567f75cf01e2b1bcd367ba
the-can-of-soup <91906096+the-can-of-soup@users.noreply.github.com>
2026-02-04 21:00:14 -0500

    Split before and after step event into two (2/2)

--- src/engine/runtime.js
+++ src/engine/runtime.js
@@ -1182,6 +1182,22 @@ class Runtime extends EventEmitter {
     }
 
     /**
+     * Event name for before the sequencer considers a thread to be stepped.
+     * @const {string}
+     */
+    static get BEFORE_THREAD_CONSIDERED () {
+        return 'BEFORE_THREAD_CONSIDERED'
+    }
+
+    /**
+     * Event name for after the sequencer considers a thread to be stepped.
+     * @const {string}
+     */
+    static get AFTER_THREAD_CONSIDERED () {
+        return 'AFTER_THREAD_CONSIDERED'
+    }
+
+    /**
      * Event name for before the sequencer steps a thread.
      * @const {string}
      */
