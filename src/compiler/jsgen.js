674b0eba093ee261b4b4ca8c6c5388cad552f093
jwklong <72522395+jwklong@users.noreply.github.com>
2026-05-13 08:03:54 +0100

    sensing_set_of let variables be set with non-strings

--- src/compiler/jsgen.js
+++ src/compiler/jsgen.js
@@ -1986,7 +1986,7 @@ class JSGenerator {
                 const variableReference = this.localVariables.next();
                 this.source += `{\nconst ${variableReference} = ${objectReference} ? ${objectReference}.lookupVariableByNameAndType("${sanitize(property)}", "", true) : "";\n`;
                 this.source += `if (${variableReference}) `;
-                this.source += `${variableReference}.value = ${value.asString()};\n}\n`;
+                this.source += `${variableReference}.value = ${value.asUnknown()};\n}\n`;
                 break;
             }
             break;
